25a575d0b4712dee82eca4214fb8f019f9fc1a80
Jatin <chaurasiyajatin68@gmail.com>
2024-09-20 18:17:33 +0530

    commit-3

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions'
 
 export default function EditInvoiceForm({
   invoice,
@@ -16,9 +17,11 @@ export default function EditInvoiceForm({
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
-}) {
+  }) {
+   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
   return (
-    <form>
+    // Passing an id as argument won't work
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -119,5 +122,5 @@ export default function EditInvoiceForm({
         <Button type="submit">Edit Invoice</Button>
       </div>
     </form>
-  );
+  )
 }
